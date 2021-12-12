d3091e7fead95adef7fe7d2b0a1a4bf426f1aee3
theoddcompanyceo <95571453+theoddcompanyceo@users.noreply.github.com>
2021-12-12 14:45:58 +0800

    Update map.php

--- opt/panel/map.php
+++ opt/panel/map.php
@@ -19,7 +19,7 @@ if(isset($_POST['action'])) {
 ?><!doctype html>
 <html>
 <head>
-	<title>Map | MCHostPanel</title>
+	<title>Map | ColourPanel</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
