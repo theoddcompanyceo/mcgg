f54407e8ef1509e79e3f643d499e3dadc35119f0
theoddcompanyceo <95571453+theoddcompanyceo@users.noreply.github.com>
2021-12-12 14:48:29 +0800

    Update console.php

--- opt/panel/console.php
+++ opt/panel/console.php
@@ -11,7 +11,7 @@ if (empty($_SESSION['user']) || !$user = user_info($_SESSION['user'])) {
 ?><!doctype html>
 <html>
 <head>
-	<title>Console | MCHostPanel</title>
+	<title>Console | ColourPanel</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
